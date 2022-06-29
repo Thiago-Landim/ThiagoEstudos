031220ec0fcbb4e3376be336ea5d12fc482f9862
Thiago Landim <thilandim84@gmail.com>
2022-06-29 18:50:44 -0300

    commit inicial

--- src/main/java/com/clientes/controller/ClienteController.java
+++ src/main/java/com/clientes/controller/ClienteController.java
@@ -3,6 +3,8 @@ package com.clientes.controller;
 import com.clientes.model.ClienteModel;
 import com.clientes.repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,6 +31,13 @@ public class ClienteController {
     @PostMapping
     public ClienteModel salvar(@RequestBody ClienteModel clienteModel){
         return clienteRepository.save(clienteModel);
+
+    }
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        clienteRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
 
     }
 }
